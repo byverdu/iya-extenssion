3426fe72614fd68912553c3207097ed7b223c06f
Albert Vallverdu <Albert.Vallverdu@reachplc.com>
2020-05-10 01:19:00 +0100

    refactor background script to use a class

--- scripts/background.js
+++ scripts/background.js
@@ -1,29 +1,111 @@
-const { colors, text, path } = config;
+class TabEnvManager {
+  static badgeOff = {
+    color: '#ff0037',
+    text: 'OFF',
+    path: './assets/icon128.png',
+  }
+  static badgeOn = {
+    color: '#15b169',
+    text: 'ON',
+    path: './assets/icon-on.png',
+  }
+  constructor() {
+    this._setBadgeContent = this.setBadgeContent.bind(this)
+    this._getLocalStorage = this.getLocalStorage.bind(this);
+    this._setLocalStorage = this.setLocalStorage.bind(this);
+    this._onBrowserActionClicked = this.onBrowserActionClicked.bind(this);
+    this._onDisabled = this.onDisabled.bind(this);
+    this._onEnabled = this.onEnabled.bind(this);
+  }
 
-chrome.runtime.onInstalled.addListener(() => {
-  setBadgeContent({
-    text: text.off,
-    color: colors.off,
-    path: path.off
-  });
-})
+  /**
+ * @param {{text: string, color: string, path: string}} Obj
+ */
+  setBadgeContent({ text, color, path }) {
+    chrome.browserAction.setBadgeText({ text })
+    chrome.browserAction.setBadgeBackgroundColor({ color })
+    chrome.browserAction.setIcon({ path })
+  }
 
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.storage.local.get(['enabled'], ({ enabled }) => {
-    if (!enabled) {
-      setBadgeContent({
-        text: text.on,
-        color: colors.on,
-        path: path.on
+  /**
+   * 
+   * @param {string} prop 
+   */
+  getLocalStorage(prop) {
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.get([prop], (storage) => {
+        if (typeof storage[prop] !== 'undefined') {
+          resolve(storage[prop])
+        } else {
+          reject('Enabled is not defined')
+        }
       })
-      chrome.storage.local.set({ enabled: true })
-    } else {
-      setBadgeContent({
-        text: text.off,
-        color: colors.off,
-        path: path.off
-      })
-      chrome.storage.local.set({ enabled: false })
+    })
+  }
+
+  /**
+   * 
+   * @param {string} prop 
+   * @param {any} value 
+   */
+  setLocalStorage(prop, value) {
+    chrome.storage.local.set({ [prop]: value })
+  }
+
+  onDisabled() {
+    const { text, color, path } = TabEnvManager.badgeOn
+    this._setBadgeContent({
+      text,
+      color,
+      path,
+    })
+
+    this._setLocalStorage('enabled', true)
+  }
+
+  onEnabled() {
+    const { text, color, path } = TabEnvManager.badgeOff
+
+    this._setBadgeContent({
+      text,
+      color,
+      path,
+    })
+
+    this._setLocalStorage('enabled', false)
+  }
+
+  async onBrowserActionClicked() {
+    try {
+      const enabled = await this._getLocalStorage('enabled')
+
+      if (!enabled) {
+        this.onDisabled()
+      }
+      if (enabled) {
+        this.onEnabled()
+      }
+    } catch (error) {
+      console.error(error)
     }
-  })
-});
+  }
+
+  init() {
+    chrome.runtime.onInstalled.addListener(() => {
+      const { text, color, path } = TabEnvManager.badgeOff
+
+      this._setBadgeContent({
+        text,
+        color,
+        path,
+      });
+      chrome.storage.local.set({ enabled: false })
+    });
+
+    chrome.browserAction.onClicked.addListener(this._onBrowserActionClicked)
+  }
+}
+
+
+window.tabEnvManager = new TabEnvManager();
+window.tabEnvManager.init()
